4acace8c207e489be0f2eb8589a3d07c19dac322
Dasha Tarasova <tarasov1@msu.edu>
2018-01-18 20:29:53 -0500

    First commit

---
+++ bot_read.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import praw
+
+reddit=praw.Reddit('bot1')
+
+subreddit = reddit.subreddit('howyoudoin')
+
+for submission in subreddit.hot(limit=5):
+  print('Title: ', submission.title)
+  print('Text: ', submission.selftext)
+  print('Score: ', submission.score)
+  print('----------------------\n')
